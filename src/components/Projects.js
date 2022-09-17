7440636fa207f200524d8512a25a20bf96db1c4b
Divyajeet Pala <divyajeetpala@gmail.com>
2022-09-17 13:30:45 +0530

    Update Projects.js
    Added new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,39 +12,41 @@ function Projects() {
             <div className="projects-container">
                 <div className="projects-wrapper">
                     <ul className='projects-items'>
-                        <a href="https://dl-melody-generator.herokuapp.com/">
-                            <CardItem
-                                src="images/image-2.jpeg"
-                                title="Melody Generator"
-                                intro="A website using LSTM for generation of a melodious melody from the input for the user wanting some COOL new music."
-                            />
-                        </a>
-                        <a href="https://github.com/divpala1/Heart-Disease-Predictor">
-                            <CardItem
-                                src="images/image-4.jpg"
-                                title="Heart Disease Predictor"
-                                path="/"
-                                intro="A website using Machine Learning models to predict the heart condition of the dear user <3."
-                            />
-                        </a>
+                        <CardItem
+                            src="images/image-2.jpg"
+                            title="Amazon Clone"
+                            path="/"
+                            intro="An amazon clone deployed on Google firebase using React JS with an integrated recommendation system which provides personalised recommendations."
+                        />
+
+                        <CardItem
+                            src="images/image-4.jpg"
+                            title="Multi-Disease Detector"
+                            path="/"
+                            intro="A website deployed on AWS EC2 using CNN and Machine Learning models to detect multiple diseases like Covid-19, cervical cancer, and cardiovascular disease for the dear user <3."
+                        />
                     </ul>
                     <ul className='projects-items'>
-                        <a href="https://github.com/divpala1/Movie-Recommender">
-                            <CardItem
-                                src="images/image-5.jpg"
-                                title="Movie Recommender"
-                                path="/"
-                                intro="A project which implements collaborative and content-based filter to recommend AWESOME movies to the user."
-                            />
-                        </a>
-                        <a href="https://github.com/divpala1/Jarvis-Voice-Assistant-with-Chatbot">
-                            <CardItem
-                                src="images/image-7.jpg"
-                                title="Voice Assistant"
-                                path="/"
-                                intro="A voice-assistant to perform day-to-day tasks for the lazy user who doesn't want to move a finger. Also, if that user's reading this, go EXERCISE."
-                            />
-                        </a>
+                        <CardItem
+                            src="images/image-2.jpeg"
+                            title="Melody Generator"
+                            path="/"
+                            intro="A website using LSTM for generation of a melodious melody from the input for the user wanting some COOL new music."
+                        />
+
+                        <CardItem
+                            src="images/image-5.jpg"
+                            title="Movie Recommender"
+                            path="/"
+                            intro="A project which implements collaborative and content-based filter to recommend AWESOME movies to the user."
+                        />
+
+                        <CardItem
+                            src="images/image-7.jpg"
+                            title="Voice Assistant"
+                            path="/"
+                            intro="A voice-assistant to perform day-to-day tasks for the lazy user who doesn't want to move a finger. Also, if that user's reading this, go EXERCISE."
+                        />
                     </ul>
                 </div>
             </div>
